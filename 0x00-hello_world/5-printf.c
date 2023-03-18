f63bf90f0e07018ea19099768069be22e28688e3
Yassin Erraqui <yassine@M1P.local>
2023-03-18 23:19:19 +0100

    FFF

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - Entry Point for the program
+ *
+ *Description: Prints to STDOUT
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
