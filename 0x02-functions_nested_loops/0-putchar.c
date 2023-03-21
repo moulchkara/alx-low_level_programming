e39cbf696e24139b486761c079b5d49a573bbd1b
Yassin Erraqui <yassine@M1P.local>
2023-03-21 17:57:45 +0000

    FF

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point of the programme
@@ -17,7 +17,7 @@ int main(void)
 	i = 0;
 	while (str[i])
 	{
-		putchar(str[i]);
+		_putchar(str[i]);
 		i++;
 	}
 	return (0);
