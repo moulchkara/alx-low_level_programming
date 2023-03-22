7577600708145dd8d985cd039fb7e103f18c8b04
Yassin Erraqui <yassine@M1P.local>
2023-03-22 01:10:40 +0000

    FF

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of the number given
+ *
+ * @n: the number given to check its sign
+ *
+ * Return: returns 1 if `n` is positive 
+ *         returns 0 if `n` is 0
+ *	   returns -1 if `n` is negative 
+ */
+
+int print_sign(int n)
+{
+	if (n >  0)
+	{
+		_putchar('+');
+		return  (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return  (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return  (0);
+	}
+}
