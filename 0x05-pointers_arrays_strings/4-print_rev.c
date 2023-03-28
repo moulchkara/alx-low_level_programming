c48b18f141b9f9355a1188cb9a6975b929296e1f
Yassin Erraqui <yassine@M1P.local>
2023-03-28 00:40:42 +0000

    FF

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line, to stdout
+ *
+ * @str: string to print
+ *
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+		i++;	
+
+	while (i >= 0)
+		_putchar(s[i--]);
+	_putchar('\n');
+}
